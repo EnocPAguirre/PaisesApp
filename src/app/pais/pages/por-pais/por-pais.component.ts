ba2dab424ac2a233e96cbef56319dddb330350bb
enoc pineda <enocpinedaa@gmail.com>
2022-02-11 23:40:47 -0600

    Separando en componentes funcion input

--- src/app/pais/pages/por-pais/por-pais.component.ts
+++ src/app/pais/pages/por-pais/por-pais.component.ts
@@ -18,10 +18,11 @@ export class PorPaisComponent {
     private paisService: PaisService
   ) { }
 
-  buscar(){
+  buscar(termino: string){
     this.hayError = false;
+    this.termino = termino;
 
-    this.paisService.buscarPais(this.termino)
+    this.paisService.buscarPais(termino)
     .subscribe( (paises) => {
       this.paises = paises;
       
