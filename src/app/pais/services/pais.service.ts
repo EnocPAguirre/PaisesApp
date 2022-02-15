0cc176792972d57e5d72e9ef5c66eadedc27434c
enoc pineda <enocpinedaa@gmail.com>
2022-02-14 21:48:13 -0600

    Mejorando componentes

--- src/app/pais/services/pais.service.ts
+++ src/app/pais/services/pais.service.ts
@@ -21,7 +21,12 @@ export class PaisService {
   buscarCapital(termino: string): Observable<Country[]>{
     const url = `${this._apiUrl}/capital/${termino}`;
     return this.http.get<Country[]>(url);
-
   }
+
+  getPaisPorAlpha(id: string): Observable<Country>{
+    const url = `${this._apiUrl}/alpha/${id}`;
+    return this.http.get<Country>(url);
+  }
+
 
 }
