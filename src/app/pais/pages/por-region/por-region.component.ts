c1fb9b7e108e72a64c6de718b353da5fd01a6c58
enoc pineda <enocpinedaa@gmail.com>
2022-02-17 22:06:35 -0600

    Ng Class or [class]

--- src/app/pais/pages/por-region/por-region.component.ts
+++ src/app/pais/pages/por-region/por-region.component.ts
@@ -1,16 +1,24 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-por-region',
   templateUrl: './por-region.component.html',
-  styles: [
+  styles: [`
+    button {
+      margin-right: 5px
+    }
+    `
   ]
 })
-export class PorRegionComponent implements OnInit {
+export class PorRegionComponent  {
+
+  regiones: string[] = ['Africa', 'Oceania', 'Europe', 'Americas', 'Asia']
+  regionActive: string = '';
 
   constructor() { }
 
-  ngOnInit(): void {
+  activarRegion(region : string){
+    this.regionActive = region;
   }
 
 }
